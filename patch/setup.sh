802e5fa949416bd1f3532c1fec2eaad379f2c602
RedSTARO <822858602@qq.com>
2023-10-05 20:33:40 +0800

    lilac: patch: Update patch target and content for Udon

--- patch/setup.sh
+++ patch/setup.sh
@@ -20,9 +20,9 @@ work_dir=/home/r/miku/
 green_echo "Entering working dir..."
 cd "$work_dir"
 
-green_echo "Replacing 720p boot animation..."
-rm -rf vendor/miku/bootanimation/bootanimation.zip
-cp device/sony/lilac/patch/bootanimation/bootanimation.zip vendor/miku/bootanimation/bootanimation.zip
+# green_echo "Replacing 720p boot animation..."
+# rm -rf vendor/miku/bootanimation/bootanimation.zip
+# cp device/sony/lilac/patch/bootanimation/bootanimation.zip vendor/miku/bootanimation/bootanimation.zip
 
 green_echo "Adding via browser..."
 rm -rf packages/apps/via
@@ -37,8 +37,8 @@ cd "$work_dir/kernel/sony/msm8998"
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
 cd "$work_dir"
 
-green_echo "Removing internal error hint when power on..."
-git apply device/sony/lilac/patch/removePowerOnHint/ActivityTaskManagerService.java.patch
+# green_echo "Removing internal error hint when power on..."
+# git apply device/sony/lilac/patch/removePowerOnHint/ActivityTaskManagerService.java.patch
 
 green_echo "Dropping something rely on LineageOS..."
 rm -rf hardware/sony/hidl
@@ -53,18 +53,20 @@ green_echo "Step1..."
 touch packages/apps/Settings/res/xml/volume_button_music_control_gesture_settings.xml
 touch packages/apps/Settings/src/com/android/settings/gestures/VolumeButtonMusicControlGestureSettings.java
 touch packages/apps/Settings/src/com/android/settings/gestures/VolumeButtonMusicControlPreferenceController.java
-git apply device/sony/lilac/patch/volumeKeyPlaybackControl/gestures.xml.patch
-git apply device/sony/lilac/patch/volumeKeyPlaybackControl/GesturesSettingPreferenceController.java.patch
-git apply device/sony/lilac/patch/volumeKeyPlaybackControl/values_miku_strings.xml.patch
-git apply device/sony/lilac/patch/volumeKeyPlaybackControl/values-zh-rCN_miku_strings.xml.patch
+# git apply device/sony/lilac/patch/volumeKeyPlaybackControl/gestures.xml.patch
+# git apply device/sony/lilac/patch/volumeKeyPlaybackControl/GesturesSettingPreferenceController.java.patch
+# git apply device/sony/lilac/patch/volumeKeyPlaybackControl/values_miku_strings.xml.patch
+# git apply device/sony/lilac/patch/volumeKeyPlaybackControl/values-zh-rCN_miku_strings.xml.patch
 git apply device/sony/lilac/patch/volumeKeyPlaybackControl/volume_button_music_control_gesture_settings.xml.patch
 git apply device/sony/lilac/patch/volumeKeyPlaybackControl/VolumeButtonMusicControlGestureSettings.java.patch
 git apply device/sony/lilac/patch/volumeKeyPlaybackControl/VolumeButtonMusicControlPreferenceController.java.patch
 green_echo "Step2..."
 git apply device/sony/lilac/patch/volumeKeyPlaybackControl/Settings.java.patch
-git apply device/sony/lilac/patch/volumeKeyPlaybackControl/PhoneWindowManager.java.patch
+# git apply device/sony/lilac/patch/volumeKeyPlaybackControl/PhoneWindowManager.java.patch
 green_echo "Step3..."
-git apply device/sony/lilac/patch/volumeKeyPlaybackControl/PhoneWindowManager.java.2.patch
+git apply device/sony/lilac/patch/volumeKeyPlaybackControl/Udon/0001-Add-Long-press-volume-key-media-control.frameworks_base.patch
+git apply device/sony/lilac/patch/volumeKeyPlaybackControl/Udon/0001-Add-Long-press-volume-key-media-control.packages_apps_Settings.patch
+# git apply device/sony/lilac/patch/volumeKeyPlaybackControl/PhoneWindowManager.java.2.patch
 green_echo "Step4..."
 touch packages/apps/Settings/res/raw-night/lottie_volume_button_music_control.json
 touch packages/apps/Settings/res/raw/lottie_volume_button_music_control.json
