98255a67f4d5e9e99514641daaab7fd3d306af4b
RedSTARO <822858602@qq.com>
2023-10-29 10:28:51 +0800

    Revert "lilac: patch: Remove extra patch."
    This reverts commit ff8ad22127eb5d6066d84a9f1c25f66bef3560b5.

--- patch/setup.sh
+++ patch/setup.sh
@@ -33,13 +33,44 @@ cd "$work_dir"
 green_echo "Dropping something rely on LineageOS..."
 rm -rf hardware/sony/hidl
 rm -rf hardware/sony/amplifier
-git apply device/sony/lilac/patch/removeLOSStuff/XperiaParts_Android.bp.patch
+# git apply device/sony/lilac/patch/removeLOSStuff/XperiaParts_Android.bp.patch
 
 green_echo "Removing su stuff in source code..."
 rm -rf system/extras/su
 
 green_echo "Apply Udon patches..."
-git apply $work_dir/device/sony/lilac/patch/Udon/device_qcom_sepolicy-legacy-um_legacy_vendor_common/device.te.patch
+# Set patch stored dir
+source_dir="$work_dir/device/sony/lilac/patch/Udon"
+
+# Set mapping of dirs
+declare -A target_dirs
+target_dirs["bionic"]="$work_dir/bionic"
+target_dirs["build_make"]="$work_dir/build/make"
+target_dirs["frameworks_base"]="$work_dir/frameworks/base"
+target_dirs["frameworks_libs_net"]="$work_dir/frameworks/libs/net"
+target_dirs["frameworks_opt_telephony"]="$work_dir/frameworks/opt/telephony"
+target_dirs["packages_modules_Connectivity"]="$work_dir/packages/modules/Connectivity"
+target_dirs["system_bpf"]="$work_dir/system/bpf"
+target_dirs["system_libhidl"]="$work_dir/system/libhidl"
+target_dirs["system_netd"]="$work_dir/system/netd"
+target_dirs["device_qcom_sepolicy-legacy-um_legacy_vendor_common"]="$work_dir/device/qcom/sepolicy-legacy-um/legacy/vendor/common"
+
+# Apply patches
+for dir in "${!target_dirs[@]}"; do
+  source_patch_dir="$source_dir/$dir"
+  target_patch_dir="${target_dirs[$dir]}"
+  if [ -d "$source_patch_dir" ] && [ -d "$target_patch_dir" ]; then
+    green_echo "Applying patches in $dir directory..."
+    cd "$target_patch_dir"
+    for patch_file in "$source_patch_dir"/*.patch; do
+      patch -p1 < "$patch_file"
+    done
+    cd -
+  else
+    green_echo "Source or target directory not found for $dir, skipping..."
+  fi
+done
 cd $work_dir
+green_echo "Patch application complete."
 
 green_echo "Setup finished, now u can start compile ur MikuUI~"
