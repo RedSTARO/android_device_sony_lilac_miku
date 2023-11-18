ec1e8f9367d1fa25aa73a3ddb26ac67d04cbd241
RedSTARO <822858602@qq.com>
2023-11-18 20:46:48 +0800

    lilac: patch: update script for KernelSU

--- patch/setup.sh
+++ patch/setup.sh
@@ -20,18 +20,16 @@ work_dir=/home/r/miku/
 green_echo "Entering working dir..."
 cd "$work_dir"
 
-green_echo "Updating kernelSU..."
+green_echo "Updating KernelSU..."
 cd "$work_dir/kernel/sony/msm8998"
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
 cd "$work_dir"
+rm -rf system/extras/su
 
 green_echo "Dropping something rely on LineageOS..."
 rm -rf hardware/sony/hidl
 rm -rf hardware/sony/amplifier
 git apply device/sony/lilac/patch/removeLOSStuff/XperiaParts_Android.bp.patch
-
-green_echo "Removing su stuff in source code..."
-rm -rf system/extras/su
 
 green_echo "Removing fingerprint mismatch hint..."
 git apply device/sony/lilac/patch/frameworks_base/0001-build-Don-t-check-for-fingerprint-mismatch.patch
